81e42b5291ad78fdd7249ae3ff36dd24373bf214
JesacaLin <JesacaLin@gmail.com>
2023-04-11 23:38:24 -0400

    lesson 1.3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { useState } from "react";
 import ToDoList from "./TodoList";
 import AddTodoForm from "./AddTodoForm";
 
 function App() {
+  const [newTodo, setNewTodo] = useState("");
   return (
     <>
       <h1>Project Tracker App</h1>
-      <AddTodoForm />
+      <AddTodoForm onAddTodo={setNewTodo} />
+      <p>{newTodo}</p>
       <ToDoList />
     </>
   );
